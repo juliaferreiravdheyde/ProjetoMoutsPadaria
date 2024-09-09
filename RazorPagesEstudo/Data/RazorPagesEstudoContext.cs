51bc3558c44be9fd9c4d789cbe818de3eb92b263
julia <jualiavdheyde@icloud.com>
2024-09-09 14:11:26 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesEstudo/Data/RazorPagesEstudoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesEstudo.Models;
+
+namespace RazorPagesEstudo.Data
+{
+    public class RazorPagesEstudoContext : DbContext
+    {
+        public RazorPagesEstudoContext (DbContextOptions<RazorPagesEstudoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesEstudo.Models.Produto> Produto { get; set; } = default!;
+    }
+}
---
+++ RazorPagesEstudo/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesEstudo.Data;
+using System.Globalization;
+namespace RazorPagesEstudo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<RazorPagesEstudoContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesEstudoContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesEstudoContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            var defaultCulture = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
