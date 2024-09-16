eeca5f9eeb02cf10589bec9b2d38174bef15bb0a
Matheus Avelino <avelinosantos09@gmail.com>
2024-09-16 16:51:08 -0300

    feat: API pontos fidelidade

---
+++ PontoFidelidadeAPI/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace PontoFidelidadeAPI.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int PontosFidelidade { get; set; }
+    }
+}
