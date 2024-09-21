b176f04a2d45b97e90e0bb8f5a775eba620cafb4
julia <jualiavdheyde@icloud.com>
2024-09-20 21:44:14 -0300

    julia-branch ajustando js e css

--- RazorPagesEstudo/wwwroot/js/site.js
+++ RazorPagesEstudo/wwwroot/js/site.js
@@ -13,60 +13,93 @@ document.querySelectorAll('input[name="selectedProducts"]').forEach((checkbox) =
     });
 });
 
-$(document).ready(function () {
-    var totalQuantity = 0;
+    $(document).ready(function () {
+        var totalQuantity = 0;
     var totalPrice = 0.0;
 
     $('#produtoSelect').on('change', function () {
-        var selectedProductId = $(this).val();
-        var selectedProductName = $("#produtoSelect option:selected").text();
-        var selectedProductPrice = parseFloat($("#produtoSelect option:selected").data("preco"));
+            var selectedProductId = $(this).val();
+    var selectedProductName = $("#produtoSelect option:selected").text();
+    var selectedProductPrice = parseFloat($("#produtoSelect option:selected").data("preco"));
 
-        var existingProduct = $('#produtoList').find('[data-product-id="' + selectedProductId + '"]');
-        if (existingProduct.length > 0) {
-            var quantityInput = existingProduct.find('.quantity');
-            var newQuantity = parseInt(quantityInput.val()) + 1;
-            quantityInput.val(newQuantity);
-            totalQuantity += 1;
-            totalPrice += selectedProductPrice;
-        } else {
-            $('#produtoList').append(`
-                <div class="form-check d-flex align-items-center mb-3" data-product-id="${selectedProductId}">
-                    <span class="me-2">${selectedProductName}</span>
-                    <input type="number" name="quantidades" class="form-control quantity" value="1" readonly />
-                    <input type="hidden" name="selectedProducts" value="${selectedProductId}" />
-                </div>
-            `);
-            totalQuantity += 1;
-            totalPrice += selectedProductPrice;
-        }
+    var existingProduct = $('#produtoList').find('[data-product-id="' + selectedProductId + '"]');
+            if (existingProduct.length > 0) {
 
-        $('#totalQuantity').text(totalQuantity);
-        $('#totalPrice').text(totalPrice.toFixed(2));
-    });
+                var quantityInput = existingProduct.find('.quantity');
+                var newQuantity = parseInt(quantityInput.val()) + 1;
+                quantityInput.val(newQuantity);
+                totalQuantity += 1;
+                totalPrice += selectedProductPrice;
+            } else {
+
+                $('#produtoList').append(`
+                    <div class="form-check d-flex align-items-center mb-3" data-product-id="${selectedProductId}">
+                        <span class="me-2">${selectedProductName}</span>
+                        <input type="number" name="quantidades" class="form-control quantity" value="1" min="1" />
+                        <button type="button" class="btn btn-danger btn-sm ms-2 remove-product">
+                            <i class="fas fa-trash"></i> <!-- Font Awesome trash icon -->
+                        </button>
+                        <input type="hidden" name="selectedProducts" value="${selectedProductId}" />
+                    </div>
+                `);
+
+    totalQuantity += 1;
+    totalPrice += selectedProductPrice;
+            }
+
+    $('#totalQuantity').text(totalQuantity);
+    $('#totalPrice').text(totalPrice.toFixed(2));
+        });
+
+    $('#produtoList').on('change', '.quantity', function () {
+            var quantityChange = parseInt($(this).val());
+    var productId = $(this).closest('[data-product-id]').data('product-id');
+    var productPrice = parseFloat($("#produtoSelect option[value='" + productId + "']").data("preco"));
+
+    var currentTotal = totalPrice - (parseInt($(this).attr('data-old-quantity')) * productPrice);
+    totalPrice = currentTotal + (quantityChange * productPrice);
+
+    $(this).attr('data-old-quantity', quantityChange);
+
+            totalQuantity = $('#produtoList').find('.quantity').toArray().reduce((total, input) => total + parseInt($(input).val()), 0);
+                            $('#totalQuantity').text(totalQuantity);
+                            $('#totalPrice').text(totalPrice.toFixed(2));
+                            });
+
+    $('#produtoList').on('click', '.remove-product', function () {
+            var quantityInput = $(this).siblings('.quantity');
+            var quantityToRemove = parseInt(quantityInput.val());
+            var productPrice = parseFloat($("#produtoSelect option[value='" + $(this).closest('[data-product-id]').data('product-id') + "']").data("preco"));
+
+    totalQuantity -= quantityToRemove;
+    totalPrice -= (quantityToRemove * productPrice);
+
+    $(this).closest('[data-product-id]').remove();
+
+    $('#totalQuantity').text(totalQuantity);
+    $('#totalPrice').text(totalPrice.toFixed(2));
+        });
 
     $('#openModal').on('click', function () {
         $('#paymentModal').modal('show');
-    });
-
+        });
 
     $('#confirmPayment').on('click', function () {
+            var formaPagamento = $('#formaPagamento').val();
+            var cpfCnpj = $('#cpfCnpj').val();
 
-        var formaPagamento = $('#formaPagamento').val();
-        var cpfCnpj = $('#cpfCnpj').val();
-
-        $('<input>').attr({
-            type: 'hidden',
-            name: 'formaPagamento',
-            value: formaPagamento
-        }).appendTo('#vendaForm');
+    $('<input>').attr({
+        type: 'hidden',
+        name: 'formaPagamento',
+        value: formaPagamento
+            }).appendTo('#vendaForm');
 
         $('<input>').attr({
             type: 'hidden',
             name: 'cpfCnpj',
             value: cpfCnpj
-        }).appendTo('#vendaForm');
+            }).appendTo('#vendaForm');
 
-        $('#vendaForm').submit();
+            $('#vendaForm').submit();
+        });
     });
-});
